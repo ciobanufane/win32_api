852dcfa6b32407ad48f599ab496598a970372b5c
Ciobanu Fane <ciobanufane17@gmail.com>
2021-03-20 09:47:43 -0700

    Added file for common functions used by entities

---
+++ win32_api/helper_functions.h
@@ -0,0 +1,80 @@
+#ifndef HELPER_FUNCTIONS_H
+#define HELPER_FUNCTIONS_H
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#include <algorithm>
+#include <vector>
+#include <WTypesbase.h>
+#include <memory>
+#include <functional>
+
+namespace helper {
+
+	inline bool compareName(LPCTSTR psz1, LPCTSTR psz2) {
+		return lstrcmpi(psz1, psz2) < 0 ? true : false;
+	}
+
+	template <typename T>
+	bool compareEntity(T e1, T e2) {
+		return lstrcmpi(e1.getName(), e2.getName()) < 0 ? true : false;
+	}
+
+	template <typename T>
+	int searchForEntityName(std::vector<T>& entities, LPCTSTR name) {
+		unsigned int length{ entities.size() };
+		unsigned int left{ 0 };
+		unsigned int right{ length - 1 };
+		unsigned int midpoint;
+		int res;
+
+		if (length == 0) {
+			return -1;
+		}
+
+		while (left <= right) {
+			midpoint = left + (right - left) / 2;
+			res = lstrcmpi(entities[midpoint].getName(), name);
+			if (res == 0) {
+				return midpoint;
+			}
+			else if (res > 0) {
+				right = midpoint - 1;
+			}
+			else {
+				left = midpoint + 1;
+			}
+		}
+		return -1;
+	}
+
+	template <typename T>
+	bool testForDuplicateNames(std::vector<T>& scores, std::function<bool(LPCTSTR, LPCTSTR)> comparator) {
+		unsigned int index{ 0 };
+		std::vector<LPCTSTR> copy;
+
+		for (typename std::vector<T>::iterator it = scores.begin(); it != scores.end(); ++it) {
+			copy.push_back(it->getName());
+			if (it->getNewName()) {
+				copy.push_back(it->getNewName());
+			}
+		}
+
+		std::sort(copy.begin(), copy.end(), comparator);
+
+		// Don't need to check the last element
+		for (; index < copy.size() - 1; ++index) {
+			if (lstrcmpi(copy[index], copy[index + 1]) == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+#endif
